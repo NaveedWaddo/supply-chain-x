1de5b35f94a8850af1bac7f3671244ac29224c96
Naveed Waddo <naveedwaddo1123@gmail.com>
2024-07-19 20:23:58 +0530

    inventry functionality

--- libs/forms/src/schemas.ts
+++ libs/forms/src/schemas.ts
@@ -15,3 +15,43 @@ export const formSchemaSignIn = formSchemaRegister.pick({
 export const formSchemaCreateItem = z.object({
   name: z.string().min(2),
 })
+
+export const addressSchema = z.object({
+  latitude: z.number(),
+  longitude: z.number(),
+  address: z.string().min(2),
+})
+
+export const formSchemaCreateWarehouse = z.object({
+  name: z.string().min(2),
+  description: z.string().optional(),
+  manufacturerId: z.string().optional(),
+  distributorId: z.string().optional(),
+  retailerId: z.string().optional(),
+  address: addressSchema,
+})
+
+export const formSchemaCreateProduct = z.object({
+  name: z.string(),
+  description: z.string().optional(),
+  image: z.any().optional(),
+})
+
+export const formSchemaUpsertInventory = z.object({
+  productId: z.number(),
+  quantity: z.number().min(1),
+  warehouseId: z.number(),
+})
+
+export const formSchemaTransferInventory = z.object({
+  productId: z.number(),
+  quantity: z.number().min(1),
+  fromWarehouseId: z.number(),
+  toWarehouseId: z.number(),
+})
+
+export const formSchemaSellInventory = z.object({
+  productId: z.number(),
+  quantity: z.number().min(1),
+  warehouseId: z.number(),
+})
